1fc2ac1097c6c1c1bc4a24898052f91afc44868d
EvgeniyaBaklakova <121979488+EvgeniyaBaklakova@users.noreply.github.com>
2023-02-03 21:05:33 +0300

    add new  line

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello? world sdfsdf");
         System.out.println("He");
+        System.out.println("He1");
     }
 }
